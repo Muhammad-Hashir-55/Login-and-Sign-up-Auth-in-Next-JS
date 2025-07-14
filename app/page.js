6d454bb20b028676de369a8995926070bcb106f2
Muhammad Hashir Awaiz <167025341+Muhammad-Hashir-55@users.noreply.github.com>
2025-07-14 16:09:55 +0500

    Update page.js and Resolving Problem

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-6">
-      <h1 className="text-3xl font-bold">Welcome to Hashir's Authentication and Login and Sign up App</h1>
+      <h1 className="text-3xl font-bold">Welcome to Hashir`s Authentication and Login and Sign up App</h1>
       <button onClick={() => router.push('/login')} className="bg-blue-600 text-white px-6 py-2 rounded">Login</button>
       <button onClick={() => router.push('/signup')} className="bg-green-600 text-white px-6 py-2 rounded">Sign Up</button>
     </div>
